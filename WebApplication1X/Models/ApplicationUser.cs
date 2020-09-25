c3e2573b0ada262ab7583ff2b2d94e8d818c0943
Patrick Daniel <patrickhdaniel@outlook.com>
2020-09-24 20:25:09 -0500

    Add project files.

---
+++ WebApplication1X/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1X.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
